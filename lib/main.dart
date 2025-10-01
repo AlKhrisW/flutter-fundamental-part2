a834d04a0f61cdabb9ecd8b6580a165ee15d5110
AlKhrisW <aldokhrisna@gmail.com>
2025-10-01 09:28:26 +0700

    Modifikasi main (Implementasi title row)

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,55 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text("41"),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Nama dan NIM Anda',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Aldo Khrisna Wijaya - 2341760091'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        body: Column(
+          children: [
+            titleSection,
+          ],
+        )
       ),
     );
   }
